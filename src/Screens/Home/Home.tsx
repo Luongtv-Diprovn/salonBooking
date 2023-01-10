810c095f265df71571595c68ec3adf577c9bf283
DPT1004 <phuthanhdo104@gmail.com>
2023-01-10 10:04:08 +0700

    feat/pageHome/SlideBanner

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { View, StyleSheet, Text } from "react-native"
-import { useAppSelector, useAppDispatch } from '../../Redux/hookRedux'
+import { View, StyleSheet, ScrollView } from "react-native"
+import SlideBanner from './components/SlideBanner'
 
 const Home = () => {
 
-    const user = useAppSelector((state) => state.user)
 
     return (
         <View style={styles.container}>
-            <Text style={styles.txtPage}>Welcome</Text>
-            <Text>{user.userProperties.name}</Text>
+            <ScrollView horizontal={false} showsVerticalScrollIndicator={false}>
+                <SlideBanner />
+
+            </ScrollView>
         </View>
     )
 }
@@ -19,13 +20,6 @@ export default Home
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "pink",
-        alignItems: 'center',
-        justifyContent: "center"
-    },
-    txtPage: {
-        fontSize: 25,
-        fontWeight: "bold"
     }
 });
 
